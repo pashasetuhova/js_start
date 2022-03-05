b40402b1476e33f3aa1aeee428eadd7d0f8ade4f
Aleksandra <s-petuch@mail.ru>
2022-03-05 20:55:37 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 /* "use strict"; */
 
-const answers = [];
-answers[0] = prompt('Как Вас зовут?', '');
-answers[1] = prompt('Сколько Вам лет?', '');
-console.log(answers);
+const numerOfLilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numerOfLilms,
+    movies: {}, /* пустой объект */
+    actors: {},
+    genres: [], /* пустой массив */
+    privat: false
+};
+
+const a = prompt('Назование один из последних просмотренных фильмов', ''),
+      b = prompt('На сколько Вы его оцениваете', ''),
+      c = prompt('Назование один из последних просмотренных фильмов', ''),
+      d = prompt('На сколько Вы его оцениваете', '');
+
+personalMovieDB.movies[a] = b; /* записываем ответы в объект в формате 'фильм': 'оценка' */
+personalMovieDB.movies[c] = d; /* можно также использовать синтаксис personalMovieDB.movies.c, но он может работать не во всех браузерах */
+
+console.log(personalMovieDB);
