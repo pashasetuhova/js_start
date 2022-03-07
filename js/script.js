3181cfd29262f3d08b58602b519c8dd71ed3343f
Aleksandra <s-petuch@mail.ru>
2022-03-07 11:16:45 +0300

    added cycle

--- js/script.js
+++ js/script.js
@@ -10,12 +10,26 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Назование один из последних просмотренных фильмов', ''),
-      b = prompt('На сколько Вы его оцениваете', ''),
-      c = prompt('Назование один из последних просмотренных фильмов', ''),
-      d = prompt('На сколько Вы его оцениваете', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Назование один из последних просмотренных фильмов', ''),
+          b = prompt('На сколько Вы его оцениваете', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-personalMovieDB.movies[a] = b; /* записываем ответы в объект в формате 'фильм': 'оценка' */
-personalMovieDB.movies[c] = d; /* можно также использовать синтаксис personalMovieDB.movies.c, но он может работать не во всех браузерах */
+if (personalMovieDB.count < 10) {
+    console.log('Вы смотрели слишком мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы настоящий киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
